06d2de28191a2413e44997064174dcdfb0fb3cd6
HyeonWooGa <yeonwoopark22@gmail.com>
2022-09-25 00:11:48 +0900

    ✨ 파이어베이스 Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import { authService } from "../fbase";
 import AppRouter from "./Router";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
