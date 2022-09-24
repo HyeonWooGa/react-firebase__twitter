a768307a97cc08320fb35c571c2c9c611f390e57
HyeonWooGa <yeonwoopark22@gmail.com>
2022-09-24 23:55:49 +0900

    ✨ footer 추가

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import AppRouter from "./Router";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Twitter Clone - 박연우</footer>
+    </>
+  );
 }
 
 export default App;
