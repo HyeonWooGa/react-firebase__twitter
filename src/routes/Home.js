50d2d9e65d316b4cde69b0ab65b416064ee0dd2e
HyeonWooGa <yeonwoopark22@gmail.com>
2022-09-26 17:13:08 +0900

    ✨ 트윗 작성 마크업 및 DB 초기 설정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
-  return <span>Home</span>;
+  const [tweet, setTweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </>
+  );
 }
 
 export default Home;
