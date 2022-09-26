7a3f7ed782ed1d049b7715c580f8b28df1542649
HyeonWooGa <yeonwoopark22@gmail.com>
2022-09-26 17:39:51 +0900

    ✨ 트윗 작성 DB 연동 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,20 @@
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
+import { dbService } from "../fbase";
 
 function Home() {
   const [tweet, setTweet] = useState("");
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+      });
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+    setTweet("");
   };
   const onChange = (event) => {
     const {
