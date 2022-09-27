82d39823ab6e0be859252edab33643897665f48e
HyeonWooGa <yeonwoopark22@gmail.com>
2022-09-27 19:27:44 +0900

    ✨ 트윗 수정, 삭제 구현

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,62 @@
-import React from "react";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { dbService } from "../fbase";
 
 function Tweet({ tweetObj, isOwner }) {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+
+  const onClickDelete = async () => {
+    const ok = window.confirm("삭제하시겠습니까?");
+    if (ok) {
+      // delete tweet
+      await deleteDoc(doc(dbService, `tweets/${tweetObj.id}`));
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewTweet(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(doc(dbService, `tweets/${tweetObj.id}`), {
+      text: newTweet,
+    });
+    setEditing(false);
+  };
   return (
     <div>
-      <h4>{tweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Edit</button>
-          <button>Delete</button>
+          {isOwner && (
+            <>
+              <form onSubmit={onSubmit}>
+                <input
+                  type="text"
+                  placeholder="Edit your tweet"
+                  value={newTweet}
+                  required
+                  onChange={onChange}
+                />
+                <input type="submit" value="제출" />
+              </form>
+              <button onClick={toggleEditing}>취소</button>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={toggleEditing}>Edit</button>
+              <button onClick={onClickDelete}>Delete</button>
+            </>
+          )}
         </>
       )}
     </div>
