dca77dfe9564ee1e5dee07d3cbe4ad04c7e6ec85
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-01 15:17:55 +0900

    ✨ 프로필, 본인 트윗 데이터 불러오기

--- src/components/Router.js
+++ src/components/Router.js
@@ -13,7 +13,7 @@ function AppRouter({ isLoggedIn, userObj }) {
         {isLoggedIn ? (
           <>
             <Route path="/" element={<Home userObj={userObj} />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile" element={<Profile userObj={userObj} />} />
           </>
         ) : (
           <>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,31 @@
-import React from "react";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { authService } from "../fbase";
+import { authService, dbService } from "../fbase";
 
-function Profile() {
+function Profile({ userObj }) {
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
     navigate("/");
   };
+
+  const getMyTweets = async () => {
+    const q = query(
+      collection(dbService, "tweets"),
+      where("creatorId", "==", `${userObj.uid}`),
+      orderBy("createdAt", "desc")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyTweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
